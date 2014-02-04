6b5d19b9ac5619c5dfdcaac4bc1f755a6b1edc5e
Yerlen Zhubangaliyev <yz@yz.kz>
2014-02-04 12:46:42 +0600

    test action

--- application/testrest/src/Module/Index/Controller/IndexController.php
+++ application/testrest/src/Module/Index/Controller/IndexController.php
@@ -9,10 +9,23 @@ namespace Module\Index\Controller
 {
     class IndexController extends \App\Controller\RestController
     {
+        /**
+         * @Access("public")
+         * @return array
+         */
         public function indexAction()
         {
             return ["test"];
         }
 
+        /**
+         * @Access("public")
+         * @param $id
+         * @return array
+         */
+        public function testAction($id)
+        {
+            return ["test-id" => $id];
+        }
     }
 }
